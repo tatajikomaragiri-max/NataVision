1602f7198f27fb2ddf22ea5cd73f1cfc9128d065
NATA Vision Admin <tatajikomaragiri@gmail.com>
2026-02-05 16:31:48 +0530

    Configure_DB_for_cloud

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,13 +3,22 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-});
+const poolConfig = process.env.DATABASE_URL
+  ? {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }
+  : {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  };
+
+const pool = new Pool(poolConfig);
 
 pool.on("connect", () => {
   console.log("Connected to the database");
